25e0e904d5e48a761758cdfac09b95cb5557ed2f
alexandrup <alexandrup@ensemble.com>
2022-04-04 16:17:38 +0300

    Chai in use

--- test/example.test.js
+++ test/example.test.js
@@ -9,3 +9,14 @@ describe('Simple Math Test', () => {
            assert.equal(3 * 3, 9)
     })
 });
+
+const expect = require('chai').expect
+
+describe('Simple Math Test', () => {
+    it('should return 2', () => {
+           expect(1 + 1).to.equal(2)
+    })
+    it('should return 9', () => {
+           expect(3 * 3).to.equal(9)
+    })
+})
